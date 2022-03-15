673c6bdd63ac2b2b20bee43fac0a07d271f1af25
pdongare142 <praveen.quagnito@gmail.com>
2022-03-15 19:18:37 +0530

    rrwer

--- src/NewDisplay.java
+++ src/NewDisplay.java
@@ -3,7 +3,7 @@ public class NewDisplay {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is display class. New Display");
+		System.out.println("This is display class. New 123");
 	}
 
 }
