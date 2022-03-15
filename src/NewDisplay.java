ef6d4004a7dc6490b518cbe844d68a9859b2fc50
pdongare142 <praveen.quagnito@gmail.com>
2022-03-15 19:13:34 +0530

    Test1

---
+++ src/NewDisplay.java
@@ -0,0 +1,9 @@
+
+public class Display {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is display class. Edited by Praveen");
+	}
+
+}
