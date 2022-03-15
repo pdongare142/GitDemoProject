4413ba4fbc9e0b8d74494e9eb48ccfb02c5f71d7
pdongare142 <praveen.quagnito@gmail.com>
2022-03-15 16:56:50 +0530

    Display updated

--- src/Display.java
+++ src/Display.java
@@ -3,7 +3,7 @@ public class Display {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is display class");
+		System.out.println("This is display class. Edited Display comment");
 	}
 
 }
