c7b7dbec849fdf1047fc27399c2b644f34206331
pdongare142 <praveen.quagnito@gmail.com>
2022-03-15 19:00:50 +0530

    Re test

--- src/Display.java
+++ src/Display.java
@@ -3,7 +3,7 @@ public class Display {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is display class. Edited Display comment");
+		System.out.println("This is display class. Edited by Praveen");
 	}
 
 }
